fbb51894e00cb66beef1fb918b58972a0e807041
paranoidq <paranoid.qian@gmail.com>
2018-05-15 17:30:52 +0800

    Initial commit

---
+++ src/main/java/me/webapp/domain/User.java
@@ -0,0 +1,8 @@
+package me.webapp.domain;
+
+/**
+ * @author paranoidq
+ * @since 1.0.0
+ */
+public class User {
+}
---
+++ src/main/java/me/webapp/web/controller/IndexController.java
@@ -0,0 +1,11 @@
+package me.webapp.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author paranoidq
+ * @since 1.0.0
+ */
+@Controller
+public class IndexController {
+}
